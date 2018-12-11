a2fa8e61658b059754085115e5b337bd8585087c
eryktrzeciakiewicz <eryk.trzeciakiewicz@gmail.com>
2018-12-11 23:44:20 +0100

    refactored

--- matrix_multiplication.py
+++ matrix_multiplication.py
@@ -5,30 +5,23 @@ import time
 
 free_row = 0
 
-def getRandomMatrix(size):
+def randomMatrix(size):
 	matrix = [[random.randint(0, 1) for x in range(size)] for y in range(size)]
 	return matrix
-	
-def printMatrix(matrix, size):
-	for row in matrix:
-		for number in row:
-			print(number, end="   ")
-		print("\n")
-	print("\n")
 
 def multiplyThreaded(first, second, size, numOfThreads):
 	lock = threading.Lock()
 	result = [[0 for x in range(size)] for y in range(size)]
 	threads = []
 	def processRow(r, size):
-		row = r
 		for column in range(size):
 			sum = 0
 			for num in range(size):
-				sum = sum or (first[row][num] and second[num][column])
+				sum = sum or (first[r][num] and second[num][column])
 				if sum == 1:
 					break
-			result[row][column] = sum
+			result[r][column] = sum
+			
 	def threadTarget():
 		global free_row
 		while free_row < size:
@@ -47,16 +40,14 @@ def multiplyThreaded(first, second, size, numOfThreads):
 	
 	return result
 	
-	
 if __name__ == "__main__":
 	size = int(sys.argv[1])
 	numOfThreads = int(sys.argv[2])
-	a = getRandomMatrix(size)
-	b = getRandomMatrix(size)
+	a = randomMatrix(size)
+	b = randomMatrix(size)
 	start = time.time()
 	res1 = multiplyThreaded(a, b, size, 1)
 	print("Normal calculation took",time.time() - start, "s")
 	start = time.time()
 	res2 = multiplyThreaded(a, b, size, numOfThreads)
 	print("Threaded multiplication with",numOfThreads,"threads took",time.time() - start,"s")
-	
